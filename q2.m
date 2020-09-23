0ea12e69214ff40010569e157f05d4eb592de9f4
Srinivas Gorur-Shandilya <code@srinivas.gs>
2020-09-23 14:13:55 -0400

    fixed name again

--- q2.m
+++ q2.m
@@ -2,6 +2,6 @@
 close all
 x = xolotl.examples.neurons.BurstingNeuron; 
 
-x.manipulate_plot_func = {@(x) q3plot(x,'HCurrent')};
+x.manipulate_plot_func = {@(x) q23plot(x,'HCurrent')};
 
 x.manipulate({'AB.Ca'; 'I_ext';'AB.HCurrent.gbar'})
--- q3.m
+++ q3.m
@@ -2,6 +2,6 @@
 close all
 x = xolotl.examples.neurons.BurstingNeuron; 
 
-x.manipulate_plot_func = {@(x) q3plot(x,'KCa')};
+x.manipulate_plot_func = {@(x) q23plot(x,'KCa')};
 
 x.manipulate({'AB.Ca'; 'I_ext';'AB.KCa.gbar'})
