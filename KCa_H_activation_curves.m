fcdcd25c2d6c2045cf697537c4efc243e54c4099
Srinivas Gorur-Shandilya <code@srinivas.gs>
2020-09-25 08:06:48 -0400

    added new activation curves plots

---
+++ KCa_H_activation_curves.m
@@ -0,0 +1,7 @@
+
+
+x = xolotl;
+
+x.show('prinz/KCa','KCa (low Ca)')
+x.show('prinz/KCa','KCa (high Ca)',30)
+x.show('generic/HCurrent')
---
+++ NaV_Kd_activation_curves.m
@@ -0,0 +1,9 @@
+% plots activation curves of NaV, Kd
+% and KCa, HCurrent
+
+
+
+x = xolotl;
+
+x.show('prinz/Kd')
+x.show('prinz/NaV')
